43b8d6698bf43a33fe2eca0f2c6d97a4e60c245e
Nouman Fatta <nouman.fatta@bssuniversal.com>
2022-03-31 11:45:46 +0500

    make it better

--- index.js
+++ index.js
@@ -9,14 +9,9 @@ const notesRoute = require("./routes/notes");
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static("client/build"));
 app.use("/user", authRoute);
 app.use("/notes", notesRoute);
 
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
