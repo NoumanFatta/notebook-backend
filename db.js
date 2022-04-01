ee3d2c4de61ed2a279948d9ad71c6470a8742295
Nouman Fatta <nouman.fatta@bssuniversal.com>
2022-04-01 14:38:54 +0500

    make it better

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uri = 'mongodb+srv://noumanfatta:bhoolgaya@cluster0.gqtlc.mongodb.net/myDB?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://noumanfatta:bhoolgaya@cluster0.gqtlc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 mongoose.connect(uri,()=>{
     console.log('connected to db');
 });
